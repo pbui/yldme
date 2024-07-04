7f7519fb19f74514923abb7586cc1a2ecb61b697
Peter Bui <pbui@bx612.space>
2024-07-04 00:47:49 -0400

    scripts/yldme.paste.sh: copy to clipboard, send notification
    - Also supports either stdin or first argument
    - Supports both wayland (wl-copy) and xorg (xclip)
    - Only sends notification is notify-send is available

--- scripts/yldme.paste.sh
+++ scripts/yldme.paste.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-curl -s -X POST --data-binary @- https://yld.me/paste
+if [ -z "$1" ]; then
+    URL=$(curl -s -X POST --data-binary @- https://yld.me/paste?raw=1)
+else
+    URL=$(curl -s -X POST --data-binary @- https://yld.me/paste?raw=1 < "$1")
+fi
+
+if [ -z "$URL" ]; then
+    exit 1
+fi
+
+if [ -n "$WAYLAND_DISPLAY" ]; then
+    printf $URL | wl-copy
+else
+    printf $URL | xclip
+fi
+
+if command -v notify-send > /dev/null; then
+    notify-send --icon="cloud-upload" "Yld.Me Paste" "$URL"
+fi
+echo $URL
